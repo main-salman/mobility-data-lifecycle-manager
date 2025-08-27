fdb4706d03bdd94725e40b94e81ce026ae7de431
Salman Naqvi <salman.naqvi@gmail.com>
2025-08-27 12:35:54 -0400

    updating app

--- update_ec2.sh
+++ update_ec2.sh
@@ -23,7 +23,7 @@ echo ""
 
 # --- CONFIGURATION ---
 EC2_USER=ec2-user
-EC2_HOST=44.201.35.100
+EC2_HOST=44.222.74.245
 EC2_KEY=salman-dev.pem
 PROJECT_DIR=/home/ec2-user/mobility-data-lifecycle-manager/   # Change if your project is in a different location
 
